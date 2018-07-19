e73c4bf66bf1808b1a9984da919153d2bca08e3e
dikhit2 <dikhit2@illinois.edu>
2018-07-18 19:05:02 -0700

    Q005Test

---
+++ src/test/java/org/anshuman/leetcode/Q005Test.java
@@ -0,0 +1,45 @@
+package org.anshuman.leetcode;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class Q005Test {
+	
+	Q005 q5;
+
+	@Before
+	public void setUp() throws Exception {
+		q5 = new Q005();
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testPass() {
+		String test = "cbbd";
+		String result = q5.longestPalindrome(test);
+		assertTrue(result.equals("bb"));
+	}
+	
+	@Test
+	public void testStringWithMultiplePalindromes() {
+		String test = "babad";
+		String result = q5.longestPalindrome(test);
+		assertTrue(result.equals("aba"));
+	}
+	
+	@Test
+	public void testStringWithNoPalindromes() {
+		String test = "abcdefg";
+		String result = q5.longestPalindrome(test);
+		assertTrue(result.equals("g")); //algorithm considers single letters as palindromes
+	}
+	
+	
+
+}
