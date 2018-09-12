31f4e8dbaa61cc07031873f75d5a8c031f046a51
dikhit2 <dikhit2@illinois.edu>
2018-09-12 08:56:24 -0700

    more test cases for problem 112

--- src/test/java/org/anshuman/leetcode/Q112Test.java
+++ src/test/java/org/anshuman/leetcode/Q112Test.java
@@ -37,5 +37,22 @@ public class Q112Test {
 		boolean result = q112.hasPathSum(root, 22);
 		assertTrue(result == true);
 	}
+	
+	@Test
+	public void testFail() {
+		TreeNode root = new TreeNode(7);
+		root.left = new TreeNode(4);
+		root.left.left = new TreeNode(11);
+		root.left.left.left = new TreeNode(7);
+		root.left.left.right = new TreeNode(2);
+		
+		root.right = new TreeNode(8);
+		root.right.left = new TreeNode(13);
+		root.right.right = new TreeNode(4);
+		root.right.right.right = new TreeNode(1);
+		
+		boolean result = q112.hasPathSum(root, 22);
+		assertTrue(result == false);
+	}
 
 }
