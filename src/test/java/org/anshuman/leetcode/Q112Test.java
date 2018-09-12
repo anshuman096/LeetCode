59d0704d0dd8110cd3e6ed5963523388404a8630
dikhit2 <dikhit2@illinois.edu>
2018-09-11 23:47:26 -0700

    question 112 initial test case

---
+++ src/test/java/org/anshuman/leetcode/Q112Test.java
@@ -0,0 +1,41 @@
+package org.anshuman.leetcode;
+
+import static org.junit.Assert.*;
+import org.junit.*;
+import org.anshuman.utilities.TreeNode;
+
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class Q112Test {
+	Q112 q112;
+
+	@Before
+	public void setUp() throws Exception {
+		q112 = new Q112();
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testStandard() {
+		TreeNode root = new TreeNode(5);
+		root.left = new TreeNode(4);
+		root.left.left = new TreeNode(11);
+		root.left.left.left = new TreeNode(7);
+		root.left.left.right = new TreeNode(2);
+		
+		root.right = new TreeNode(8);
+		root.right.left = new TreeNode(13);
+		root.right.right = new TreeNode(4);
+		root.right.right.right = new TreeNode(1);
+		
+		boolean result = q112.hasPathSum(root, 22);
+		assertTrue(result == true);
+	}
+
+}
