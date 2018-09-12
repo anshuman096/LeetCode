6c8ddaf8406cc0c8a3336f5bf5179d43a78ed658
dikhit2 <dikhit2@illinois.edu>
2018-09-11 23:42:42 -0700

    Q21 test cases

--- src/test/java/org/anshuman/leetcode/Q021Test.java
+++ src/test/java/org/anshuman/leetcode/Q021Test.java
@@ -40,6 +40,68 @@ public class Q021Test {
 		}
 	}
 	
+	@Test
+	public void testDuplicateValues() {
+		ListNode l1 = new ListNode(1);
+		ListNode l2 = new ListNode(1);
+		ListNode l3 = new ListNode(2);
+		
+		l1.next = l2;
+		l2.next = l3;
+		
+		ListNode l4 = new ListNode(1);
+		ListNode l5 = new ListNode(4);
+		ListNode l6 = new ListNode(10);
+		
+		l4.next = l5;
+		l5.next = l6;
+		
+		ListNode mergedHead = q21.mergeTwoLists(l1, l4);
+		ListNode curr = mergedHead;
+		assertEquals(curr.val, 1);
+		curr = curr.next;
+		assertEquals(curr.val, 1);
+		curr = curr.next;
+		assertEquals(curr.val, 1);
+		curr = curr.next;
+		assertEquals(curr.val, 2);
+		curr = curr.next;
+		assertEquals(curr.val, 4);
+		curr = curr.next;
+		assertEquals(curr.val, 10);
+		curr = curr.next;
+	}
+	
+	@Test
+	public void testDifferentLengthLists() {
+		ListNode l1 = new ListNode(1);
+		ListNode l2 = new ListNode(3);
+		ListNode l3 = new ListNode(4);
+		
+		l1.next = l2;
+		l2.next = l3;
+		
+		ListNode l4 = new ListNode(2);
+		ListNode l5 = new ListNode(5);
+		ListNode l6 = new ListNode(6);
+		ListNode l7 = new ListNode(7);
+		ListNode l8 = new ListNode(8);
+		ListNode l9 = new ListNode(9);
+		
+		l4.next = l5;
+		l5.next = l6;
+		l6.next = l7;
+		l7.next = l8;
+		l8.next = l9;
+		
+		ListNode mergedHead = q21.mergeTwoLists(l1, l4);
+		ListNode curr = mergedHead;
+		for(int i = 1; i <= 9; i++) {
+			assertEquals(curr.val, i);
+			curr = curr.next;
+		}
+	}
+	
 	
 
 }
